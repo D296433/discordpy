0207f0a73aebc01ede589abd084861cf4f5ee502
D296433 <D296433@edu.curio.nl>
2023-06-07 10:49:12 +0200

    Build in admin check system

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord
+import discord, csv
 class Client(discord.Client):
     async def on_ready(self):
         print(f"Succesfully logged on {self.user}!")
@@ -6,23 +6,34 @@ class Client(discord.Client):
 
 
 
-
+# ============================commands=============================
     async def on_message(self, message):
         print(f'{message.author}: {message.content}')
         if message.author == client.user:
             return
-        if message.content.startswith('!ping'):
-            await message.channel.send('Pong!')
+        if message.content.startswith('!admin'):
+            adminFile = open('data/admin.csv', 'r')
+            admins = csv.DictReader(adminFile)
+            admins = list(admins)
+            adminFile.close()
+            adminCheck = False
+            for admin in admins:
+                if(admin['perms']=="1" and admin['id']==str(message.author.id)):
+                    adminCheck = True
+            if(adminCheck==True):
+                await message.channel.send("You are a admin!")
+            else:
+                await message.channel.send("You are not a admin!")
 
 
 
 
 
-# intents
+# ============================intents============================
 intents = discord.Intents.default()
 intents.message_content = True
 
-# start bot
+# ============================start bot============================
 client = Client(intents=intents)
-token = open('token.txt', 'r').read()
+token = open('data/token.txt', 'r').read()
 client.run(token)
